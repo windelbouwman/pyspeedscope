e78ad5e7cd362dcfc944dbca0a56c314c1abaedd
Eugene Gubenkov <gubenkoved@gmail.com>
2021-06-22 16:41:23 +0400

    Compatibility with python < 3.3

--- demo.py
+++ demo.py
@@ -5,19 +5,19 @@ import time
 
 def func1(x):
     for y in range(15):
-        # print(f'func1 {x} - {y}')
+        # print('func1 %s - %s' % (x, y))
         time.sleep(0.001)
 
 def func2(x):
     for y in range(4):
-        # print(f'func2 {x} - {y}')
+        # print('func2 %s - %s' % (x, y))
         func1(x)
         time.sleep(0.002)
 
 def main():
     for x in range(3):
         time.sleep(0.003)
-        print(f'standby! {x}')
+        print('standby! %s' % x)
         func1(x)
         func2(x)
 
